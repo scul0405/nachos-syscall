f8fd7033b49e4d795a723a11301b1ba7a62949f3
hkhangus <huukhangtc@gmail.com>
2023-11-18 23:41:30 -0800

    create help program

---
+++ nachos-3.4/code/test/help.c
@@ -0,0 +1,11 @@
+/* help.c 
+ *    Test program to print information about team, describe about program sort and ascii.
+ */
+
+#include "syscall.h"
+
+int
+main(){
+	PrintString("help program")
+	return 0;
+}
