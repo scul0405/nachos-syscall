b14ee8785485782b19a95316d2493954d10ba655
hkhangus <huukhangtc@gmail.com>
2023-11-20 19:59:05 -0800

    Add program help

--- nachos-3.4/code/test/help.c
+++ nachos-3.4/code/test/help.c
@@ -6,6 +6,15 @@
 
 int
 main(){
-	PrintString("help program")
+	
+	PrintString("--THONG TIN NHOM--\n");
+	PrintString("\t21120262 - Nguyen Huynh Huu Khang\n");
+	PrintString("\t21120300 - Phan Thanh Ha Nguyen\n");
+	PrintString("\t21120353 - Vi Ly Duy Truong\n");
+	PrintString("\t21120359 - Pham Quang Tuyen\n");
+	PrintString("\t21120530 - Nguyen Hoang Phuc\n\n");
+
+	PrintString("Mo ta chuong trinh ascii: ");
+	PrintString("Mo ta chuong trinh sort: ");
 	return 0;
 }
